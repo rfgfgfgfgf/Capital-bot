b1f815a7fe955d6fd3a0831807b58a88421b778a
Rfgfgfgfgf <rfgfgfgfgf3456787@gmail.com>
2025-03-01 21:57:34 +0400

    см

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telebot
+from config import TOKEN, DATABASE
+from logic import DB_Manager
+
+bot = telebot.TeleBot(TOKEN)
+manager = DB_Manager(DATABASE)  # Создаем объект для работы с БД
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет! Я бот, который может показывать столицы стран. Напиши /help для списка команд.")
+
+@bot.message_handler(commands=['help'])
+def handle_help(message):
+    bot.send_message(message.chat.id, "Доступные команды:\n/capital [страна] - показать столицу указанной страны")
+
+@bot.message_handler(commands=['capital'])
+def handle_capital(message):
+    country_name = ' '.join(message.text.split()[1:])  # Получаем название страны
+    capital = manager.get_capital(country_name)
+    
+    if capital:
+        bot.send_message(message.chat.id, f'Столица {country_name}: {capital}')
+    else:
+        bot.send_message(message.chat.id, 'Такой страны нет в базе данных или допущена ошибка в названии.')
+
+# Запуск бота
+if __name__ == "__main__":
+    bot.polling()
---
+++ config.py
@@ -0,0 +1,2 @@
+DATABASE = 'database.db'
+TOKEN = '8132196258:AAGRDvK4rJgSac8yyNkmUEYKfekyw2egda4'
